4b0dd0b86417265c0849d8d7b29571c81fa820cf
mvd4username <113714817+s1z1-balance@users.noreply.github.com>
2025-06-29 18:20:06 +0000

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ echo "Hello... Wait Please.."
 sudo apt update && sudo apt upgrade
 sudo apt install wget 
 
-mkdir server (paper 1.16.5)
-echo "Create Dir 'server (paper 1.16.5)'"
-cd server (paper 1.16.5)
+mkdir server paper 1.16.5
+echo "Create Dir 'server paper 1.16.5'"
+cd server paper 1.16.5
 
 wget https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar -O server.jar
 wget https://raw.githubusercontent.com/s1z1-balance/serversetup/refs/heads/main/eula.txt
